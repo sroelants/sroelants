af1a7b467b15bdffb0149b90e8b991e0178fb5bf
Sam Roelants <roelants.sam@gmail.com>
2019-09-23 22:14:20 +0200

    Add theme toggle transition style on first toggle.
    Removed the transition from the default style and dynamically add it on
    click. That way we avoid the toggle transition on pageload.

--- src/_includes/assets/js/toggle.js
+++ src/_includes/assets/js/toggle.js
@@ -1,12 +1,20 @@
 const themeToggle = document.getElementById('lightmode');
+const themeToggleButton = document.querySelector('.light-toggle__button');
 
+
+// Set toggle state on pageload.
 if (localStorage.theme === 'dark') {
   themeToggle.checked = true;
 } else {
   themeToggle.checked = false;
 }
 
+
 themeToggle.addEventListener('click', () => {
+  // if I set this initially, the toggle transitions on pageload.
+  // This way, the transition style gets set *after* first render.
+  themeToggleButton.style.transition = "all 0.1s ease-in-out";
+
   if(themeToggle.checked) { // dark theme has been selected
     document.documentElement.setAttribute('data-theme', 'dark');
     localStorage.setItem('theme', 'dark'); // save theme selection 
@@ -15,4 +23,6 @@ themeToggle.addEventListener('click', () => {
     localStorage.removeItem('theme'); // reset theme selection 
   } 
 });
+
+
 
